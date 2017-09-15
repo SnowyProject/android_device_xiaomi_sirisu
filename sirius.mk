3c1eec13731d3c7c22574aadee5c2e0cae8bef29
Ananth Raghavan Subramanian <sananth@codeaurora.org>
2017-09-14 22:40:38 -0700

    sirius: powerhint: Added powerhint.xml
    Added powerhint.xml for SDM710. Is parsed by PerfHAL, contains hint
    resources.
    
    Change-Id: Idb30ea26b3a987794348c4a69330571bd8dbc218

--- sirius.mk
+++ sirius.mk
@@ -292,6 +292,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.power@1.2-service-qti
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/power/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+
 # Public Libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
