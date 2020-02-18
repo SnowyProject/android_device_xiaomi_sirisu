de90efbd615e68133050fb07e73b77285eef6972
Bruno Martins <bgcngm@gmail.com>
2020-02-18 23:49:20 +0000

    sirius: Split system and vendor props
    Change-Id: If965be42e68e9ebbd816656dd2af79f541c1ceea

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -169,6 +169,11 @@ TARGET_COPY_OUT_VENDOR := vendor
 # Power
 TARGET_TAP_TO_WAKE_NODE := "/dev/input/event3"
 
+# Properties
+TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
+
 # QCOM
 BOARD_USES_QCOM_HARDWARE := true
 
