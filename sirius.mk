feb59548d03777dc49feee32359165401cb3d9eb
Bruno Martins <bgcngm@gmail.com>
2020-07-16 23:04:49 +0100

    sirius: Switch to common QTI power HAL
    There's nothing really that different here when compared to the
    common HAL except for the specific DT2W handling, which can simply
    be supported as a power feature lib.
    
    Change-Id: I4c9c2530c8090e05dab38ed829d59ece5e00e1fb

--- sirius.mk
+++ sirius.mk
@@ -290,7 +290,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service.xiaomi_sirius
+    android.hardware.power@1.2-service-qti
 
 # Public Libraries
 PRODUCT_COPY_FILES += \
