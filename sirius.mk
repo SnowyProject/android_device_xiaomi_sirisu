bdf099118c9f8f5ba0392588e26653a6dc448b7c
Bruno Martins <bgcngm@gmail.com>
2020-08-23 12:18:02 +0100

    sirius: Bring back still required dummy HIDL lib
    Wrongly removed in commit 85a7134, some blobs still depend on
    android.hidl.base@1.0.so.
    
    Change-Id: Ide84a07ab01acad8cfb4a25149a1c81034f57b69

--- sirius.mk
+++ sirius.mk
@@ -209,6 +209,11 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.0-impl \
     android.hardware.health@2.0-service
 
+# HIDL
+PRODUCT_PACKAGES += \
+    android.hidl.base@1.0 \
+    android.hidl.base@1.0.vendor
+
 # IFAA manager
 PRODUCT_PACKAGES += \
     org.ifaa.android.manager
