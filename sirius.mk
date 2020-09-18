866dad65cfa097b817d0fc900e0b6ee8933252fe
LuK1337 <priv.luk@gmail.com>
2020-09-18 14:36:05 +0200

    sirius: Build libhidltransport/libhwbinder
    * Needed for old blobs
    
    Change-Id: I43fc0e46547dfc126a4b2ac9dcc82c7c4f2ee172

--- sirius.mk
+++ sirius.mk
@@ -217,7 +217,11 @@ PRODUCT_PACKAGES += \
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
-    android.hidl.base@1.0.vendor
+    android.hidl.base@1.0.vendor \
+    libhidltransport \
+    libhidltransport.vendor \
+    libhwbinder \
+    libhwbinder.vendor
 
 # IFAA manager
 PRODUCT_PACKAGES += \
