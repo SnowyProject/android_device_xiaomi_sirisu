f7983758288af7931f5309d1febe3226b04bd79e
Michael Bestas <mkbestas@lineageos.org>
2020-12-19 02:53:35 +0200

    sirius: Switch to standalone extract utils
    Align with templates while we are at it
    
    Change-Id: Ife50623a3d1b8733f5cc76995c982d8950d39ad4

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 #
-# Copyright (C) 2018 The LineageOS Project
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017-2020 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 set -e
-
-INITIAL_COPYRIGHT_YEAR=2018
 
 # Required!
 DEVICE=sirius
@@ -17,9 +16,9 @@ VENDOR=xiaomi
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
-LINEAGE_ROOT="${MY_DIR}/../../.."
+ANDROID_ROOT="${MY_DIR}/../../.."
 
-HELPER="${LINEAGE_ROOT}/vendor/lineage/build/tools/extract_utils.sh"
+HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
     echo "Unable to find helper script at ${HELPER}"
     exit 1
@@ -27,9 +26,9 @@ fi
 source "${HELPER}"
 
 # Initialize the helper for sirius
-setup_vendor "${DEVICE}" "${VENDOR}" "${LINEAGE_ROOT}" false
+setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false
 
-# Copyright headers and guards
+# Warning headers and guards
 write_headers
 
 # The standard device blobs
