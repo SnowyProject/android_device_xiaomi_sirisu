NusantaraROM-Devices/device_xiaomi_sirius

sirius: Switch to common QTI power HAL
There's nothing really that different here when compared to the
common HAL except for the specific DT2W handling, which can simply
be supported as a power feature lib.

Change-Id: I4c9c2530c8090e05dab38ed829d59ece5e00e1fb

BoardConfig.mk
@@ -161,7 +161,7 @@ TARGET_USES_MKE2FS := true
 TARGET_COPY_OUT_VENDOR := vendor
 
 # Power
-TARGET_TAP_TO_WAKE_NODE := "/dev/input/event3"
+TARGET_POWER_SET_FEATURE_LIB := libpower_feature.xiaomi_sdm710
 
 # QCOM
 BOARD_USES_QCOM_HARDWARE := true

power/Android.mk
@@ -6,43 +6,17 @@ ifeq ($(call is-vendor-board-platform,QCOM),true)
 # hw/<POWERS_HARDWARE_MODULE_ID>.<ro.hardware>.so
 include $(CLEAR_VARS)
 
-LOCAL_MODULE_RELATIVE_PATH := hw
-LOCAL_SHARED_LIBRARIES := liblog \
-                          libcutils \
-                          libdl \
-                          libxml2 \
-                          libbase \
-                          libhidlbase \
-                          libhidltransport \
-                          libutils \
-                          android.hardware.power@1.2
-LOCAL_HEADER_LIBRARIES += libutils_headers
-LOCAL_HEADER_LIBRARIES += libhardware_headers
-LOCAL_SRC_FILES := power-common.c \
-                   metadata-parser.c \
-                   utils.c \
-                   list.c \
-                   hint-data.c \
-                   powerhintparser.c \
-                   service.cpp \
-                   Power.cpp \
-                   power-710.c
-LOCAL_C_INCLUDES := external/libxml2/include \
-                    external/icu/icu4c/source/common
-
-ifeq ($(TARGET_USES_INTERACTION_BOOST),true)
-    LOCAL_CFLAGS += -DINTERACTION_BOOST
-endif
+LOCAL_HEADER_LIBRARIES := libhardware_headers qti_powerhal_headers
+LOCAL_SRC_FILES := power-feature.c
+LOCAL_SHARED_LIBRARIES := libutils
 
 ifneq ($(TARGET_TAP_TO_WAKE_NODE),)
     LOCAL_CFLAGS += -DTAP_TO_WAKE_NODE=\"$(TARGET_TAP_TO_WAKE_NODE)\"
 endif
 
-LOCAL_MODULE := android.hardware.power@1.2-service.xiaomi_sirius
-LOCAL_INIT_RC := android.hardware.power@1.2-service.xiaomi_sirius.rc
+LOCAL_MODULE := libpower_feature.xiaomi_sdm710
 LOCAL_MODULE_TAGS := optional
-LOCAL_CFLAGS += -Wno-unused-parameter -Wno-unused-variable
 LOCAL_VENDOR_MODULE := true
-include $(BUILD_EXECUTABLE)
+include $(BUILD_STATIC_LIBRARY)
 
 endif

SnowyProject/android_device_xiaomi_sirisu

BoardConfig.mk
@@ -168,7 +168,7 @@ TARGET_USES_MKE2FS := true
 TARGET_COPY_OUT_VENDOR := vendor
 
 # Power
-TARGET_TAP_TO_WAKE_NODE := "/dev/input/event3"
+TARGET_POWER_SET_FEATURE_LIB := libpower_feature.xiaomi_sirius
 
 # Properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop

power/Android.mk
@@ -1,48 +1,16 @@
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(call is-vendor-board-platform,QCOM),true)
-
-# HAL module implemenation stored in
-# hw/<POWERS_HARDWARE_MODULE_ID>.<ro.hardware>.so
 include $(CLEAR_VARS)
 
-LOCAL_MODULE_RELATIVE_PATH := hw
-LOCAL_SHARED_LIBRARIES := liblog \
-                          libcutils \
-                          libdl \
-                          libxml2 \
-                          libbase \
-                          libhidlbase \
-                          libhidltransport \
-                          libutils \
-                          android.hardware.power@1.2
-LOCAL_HEADER_LIBRARIES += libutils_headers
-LOCAL_HEADER_LIBRARIES += libhardware_headers
-LOCAL_SRC_FILES := power-common.c \
-                   metadata-parser.c \
-                   utils.c \
-                   list.c \
-                   hint-data.c \
-                   powerhintparser.c \
-                   service.cpp \
-                   Power.cpp \
-                   power-710.c
-LOCAL_C_INCLUDES := external/libxml2/include \
-                    external/icu/icu4c/source/common
-
-ifeq ($(TARGET_USES_INTERACTION_BOOST),true)
-    LOCAL_CFLAGS += -DINTERACTION_BOOST
-endif
-
-ifneq ($(TARGET_TAP_TO_WAKE_NODE),)
-    LOCAL_CFLAGS += -DTAP_TO_WAKE_NODE=\"$(TARGET_TAP_TO_WAKE_NODE)\"
-endif
-
-LOCAL_MODULE := android.hardware.power@1.2-service.xiaomi_sirius
-LOCAL_INIT_RC := android.hardware.power@1.2-service.xiaomi_sirius.rc
+LOCAL_MODULE := libpower_feature.xiaomi_sirius
 LOCAL_MODULE_TAGS := optional
-LOCAL_CFLAGS += -Wno-unused-parameter -Wno-unused-variable
-LOCAL_VENDOR_MODULE := true
-include $(BUILD_EXECUTABLE)
 
-endif
+LOCAL_VENDOR_MODULE := true
+
+LOCAL_SRC_FILES := power-feature.c
+
+LOCAL_SHARED_LIBRARIES := libutils
+
+LOCAL_HEADER_LIBRARIES := libhardware_headers qti_powerhal_headers
+
+include $(BUILD_STATIC_LIBRARY)