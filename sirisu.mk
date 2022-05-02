6bd38dacb43a6d1c6c74ad31fe801d3ce8d6ac77
Bruno Martins <bgcngm@gmail.com>
2022-05-02 14:06:56 +0100

    sirisu: Switch to source-built mlipay interface
    Change-Id: I7daaaffd9fe7a870e42c1d882b52dc93b3231120

--- sirisu.mk
+++ sirisu.mk
@@ -327,6 +327,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml
 
+# Mlipay
+PRODUCT_PACKAGES += \
+    vendor.xiaomi.hardware.mlipay@1.1.vendor:64
+
 # Net
 PRODUCT_PACKAGES += \
     android.system.net.netd@1.1.vendor \
